02ff26b7dc536f5dd9420100bf8944ad36201fdb
깡단 <yyb19920702@gmail.com>
2026-05-11 11:35:00 +0900

    fix : Lv6 sign-up에 비밀번호 검증 추가

--- src/main/java/org/example/expert/domain/auth/dto/request/SignupRequest.java
+++ src/main/java/org/example/expert/domain/auth/dto/request/SignupRequest.java
@@ -2,6 +2,7 @@ package org.example.expert.domain.auth.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,6 +15,10 @@ public class SignupRequest {
     @NotBlank @Email
     private String email;
     @NotBlank
+    @Pattern(
+            regexp = "^(?=.*\\d)(?=.*[A-Z]).{8,}$",
+            message = "비밀번호는 8자 이상이며 숫자와 대문자를 포함해야 합니다."
+    )
     private String password;
     @NotBlank
     private String userRole;
